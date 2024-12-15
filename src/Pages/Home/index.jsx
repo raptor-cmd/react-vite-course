5f146e467a84a8b3a61c7ee0a09b62ac10224cf9
raptor-cmd <josue091201@gmail.com>
2024-12-16 00:55:43 +0100

    Layout

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,11 @@
-
+import Layout from '../../Components/Layout'
 function Home() {
 
   return (
-    <>
-      <div className='bg-red-100'>
-        Hola mundo mi ratita
-      </div>
-    </>
+        <Layout>
+          Home
+        </Layout>
+
   )
 }
 
