15e65cb5fa691cf12228fb6f149b407d02d7ac9e
raptor-cmd <josue091201@gmail.com>
2024-12-16 01:39:39 +0100

    Api call fake store

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,29 @@
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 function Home() {
+
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then(response => response.json())
+    .then(data => setItems(data))
+  }, [])
 
   return (
         <Layout>
           Home
-          <Card />
+
+          <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+            {
+            items?.map((item) => <Card key={item.id} data={item}/>)
+          }
+          </div>
+          
+          
         </Layout>
 
   )
